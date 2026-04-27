92c1c665f675f7da4431988615f73b94654f78f7
Valentina Valverde <sofficestudio@gmail.com>
2026-04-27 11:55:47 -0400

    Update "load more" button on products page to match styles across the site

--- app/components/PaginatedResourceSection.tsx
+++ app/components/PaginatedResourceSection.tsx
@@ -24,14 +24,8 @@ export function PaginatedResourceSection<NodesType>({
 
         return (
           <div>
-            <PreviousLink>
-              {isLoading ? (
-                'Loading...'
-              ) : (
-                <span>
-                  <span aria-hidden="true">↑</span> Load previous
-                </span>
-              )}
+            <PreviousLink className="mb-16 bg-black text-[#f0f2ea] border-2 border-black rounded-full px-8 h-[52px] flex flex-row items-center justify-center w-full mt-2 text-base group hover:bg-transparent hover:text-black transition-colors">
+              {isLoading ? 'Loading...' : <span>Load previous</span>}
             </PreviousLink>
             {resourcesClassName ? (
               <div
@@ -44,14 +38,8 @@ export function PaginatedResourceSection<NodesType>({
             ) : (
               resourcesMarkup
             )}
-            <NextLink>
-              {isLoading ? (
-                'Loading...'
-              ) : (
-                <span>
-                  Load more <span aria-hidden="true">↓</span>
-                </span>
-              )}
+            <NextLink className="mt-16 bg-black text-[#f0f2ea] border-2 border-black rounded-full px-8 h-[52px] flex flex-row items-center justify-center w-full mt-2 text-base group hover:bg-transparent hover:text-black transition-colors">
+              {isLoading ? 'Loading...' : <span>Load more</span>}
             </NextLink>
           </div>
         );
