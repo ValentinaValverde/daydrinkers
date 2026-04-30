3caf0e39fb604d38bd8917c9dd1c5b2739b3f782
Valentina Valverde <sofficestudio@gmail.com>
2026-04-30 15:22:52 -0400

    Decrease margin/padding on sides for mobile view (was too cramped)

--- app/components/ui/CheckeredBorder.tsx
+++ app/components/ui/CheckeredBorder.tsx
@@ -3,7 +3,6 @@ const TILE = 106;
 const checkerStyle: React.CSSProperties = {
   backgroundImage: `repeating-conic-gradient(#2a6b8f 0deg 90deg, #f0f2ea 90deg 180deg)`,
   backgroundSize: `${TILE}px ${TILE}px`,
-  padding: `${TILE}px`,
 };
 
 export default function CheckeredBorder({
@@ -12,7 +11,7 @@ export default function CheckeredBorder({
   children: React.ReactNode;
 }) {
   return (
-    <div className="w-full" style={checkerStyle}>
+    <div className="w-full px-[53px] py-[106px] md:px-[106px]" style={checkerStyle}>
       <div className="bg-[#f0f2ea]">{children}</div>
     </div>
   );
